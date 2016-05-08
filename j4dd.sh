2f14ff808a9ba3190a61ee356e7c643cda237e29
Phixion <ridelore@users.noreply.github.com>
2016-05-08 03:37:14 +0200

    Create j4dd.sh

---
+++ j4dd.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+j4-dmenu-desktop --dmenu="(cat ; (stest -flx $(echo $PATH | tr : ' ') | sort -u)) | dmenu -fn 'Ohsnapu-10' -nb '#2F343F' -nf '#9fbc00' -sb '#9fbc00' -sf '#2F343F' -l 10 -i" --term="termite"
