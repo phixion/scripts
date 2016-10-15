fcf966eb85c2522542318ee04eb83e7ed79ba160
Phixion <ridelore@users.noreply.github.com>
2016-10-15 13:08:40 +0200

    Update pacman-explicit.sh

--- pacman-explicit.sh
+++ pacman-explicit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Displays every package you've explicitly installed, excluding base and base-devel packages.  Very useful for finding packages you don't really want anymore.
-pacman -Qei | awk '/^Name/ { name=$3 } /^Groups/ { if ( $3 != "base" && $3 != "base-devel" ) { print name } }'
+pacman -Qei $(pacman -Qu|cut -d" " -f 1)|awk ' BEGIN {FS=":"}/^Name/{printf("\033[1;36m%s\033[1;37m", $2)}/^Description/{print $2}'
