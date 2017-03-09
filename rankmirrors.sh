0f62703b76881aa8cef74d06b45a8f200e1d06a5
Phixion <ridelore@users.noreply.github.com>
2017-03-09 10:47:51 +0100

    Update rankmirrors.sh

--- rankmirrors.sh
+++ rankmirrors.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /etc/pacman.d/
-cp mirrorlist mirrorlist.bak
-rankmirrors -n 6 mirrorlist.bak > mirrorlist
+cd /etc/pacman.d/ &&
+cp mirrorlist mirrorlist.bak &&
+rankmirrors -n 6 mirrorlist.bak > mirrorlist &&
 pacman -Syy
