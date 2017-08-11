05a8115a0eabaaf6e9657bc9b669174a6bf3076b
Phixion <ms@phixion.org>
2017-08-11 23:36:40 +0200

    add dome9 foo for openvpn

--- dome9_agent.sh
+++ dome9_agent.sh
@@ -12,3 +12,11 @@ sudo chmod +x /etc/init.d/dome9d
 sudo update-rc.d dome9d defaults
 dome9d pair -k <pairing key> -s <servername>
 sudo update-rc.d dome9d enable
+
+# openvpn settings for dome9
+sudo echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.d/99-sysctl.conf
+sudo sysctl -p
+sudo iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A FORWARD -i tun0 -o eth0 -s 10.8.0.0/24 -j ACCEPT
+sudo echo -e "# forwarding\n-I FORWARD -j ACCEPT" >> /usr/share/pyshared/dome9/agent/iptables.boilerplate
