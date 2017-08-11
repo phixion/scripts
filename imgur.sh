c3bcce8632f9d519057ff20d6019159e6c1d97c8
Martin Spitz <phixion@users.noreply.github.com>
2017-08-11 13:00:16 +0200

    Create imgur.sh

---
+++ imgur.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Call with '-s' to target only a selection of the screen.
+
+clientid='yourclientID'
+img=$(mktemp '/tmp/img-XXXXXX.png')
+
+scrot -z "$@" $img >/dev/null 2>&1 || exit
+res=$(curl -sH "Authorization: Client-ID $clientid" -F "image=@$img" "https://api.imgur.com/3/upload")
+
+echo $res | grep -qo '"status":200' && link=$(echo $res | sed -e 's/.*"link":"\([^"]*\).*/\1/' -e 's/\\//g')
+test -n "$link" && (printf $link | xclip; printf "\a" && rm "$img") || echo "$res" > "$img.error"
