b3053b81a8025240641c808540c6fe905871090f
Phixion <ms@phixion.org>
2017-09-16 07:55:02 +0200

    small script to list installed opkg packages
    for easier updating

---
+++ opkg_installed.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#collect installed openwrt packages
+#reinstall after update with opkg install `cat /etc/config/packlist.txt`
+
+FLASH_TIME=$(opkg info busybox | grep '^Installed-Time: ')
+
+for i in $(opkg list-installed | cut -d' ' -f1)
+do
+    if [ "$(opkg info $i | grep '^Installed-Time: ')" != "$FLASH_TIME" ]
+    then
+        echo $i
+    fi
+done
