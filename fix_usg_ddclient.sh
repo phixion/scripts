ecb912dde60c7baaf0d47cc81ff373707341da5e
Martin Spitz <m@rtin.ms>
2021-01-08 00:34:37 +0100

    Update fix_usg_ddclient.sh

--- fix_usg_ddclient.sh
+++ fix_usg_ddclient.sh
@@ -11,13 +11,13 @@ grep -q -F "$DEB_REPO" "$APT_SRC" || echo "$DEB_REPO" >> "$APT_SRC"
 # Run Apt update
 apt-get update; apt-get -y install libdata-validate-ip-perl
 
-# Download new ddclient and replace the existing version
+# Download working ddclient and replace the existing version
 cd /tmp
-curl -L -O https://raw.githubusercontent.com/ddclient/ddclient/master/ddclient
+curl -L -O https://raw.githubusercontent.com/phixion/scripts/master/ddclient
 cp /usr/sbin/ddclient /usr/sbin/ddclient.bkp
 cp ddclient /usr/sbin/ddclient
 chmod +x /usr/sbin/ddclient
 
 # Tell the USG to update configuration and then display the status
 # Run the following manually to update your DDNS record:
-# update dns dynamic interface eth0 && sleep 20 && show dns dynamic status
+update dns dynamic interface pppoe0 && sleep 20 && show dns dynamic status
